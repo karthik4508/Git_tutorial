ae6598f87a4e10bf5150eaf36253448e3c69e48e
karthik4508 <oruguntakarthi@gmail.com>
2024-10-01 15:41:14 +0530

    Second commit

---
+++ basics/Open_Edge_browser.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+driver = webdriver.Edge()
+driver.get("https://www.fb.com")
+driver.maximize_window()
+driver.implicitly_wait(30)
+print(driver.title)
+driver.close()
